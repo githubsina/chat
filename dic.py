8138438d2d26dac760320c390df42629abfddd4b
Caroline <shanqinglan1@163.com>
2020-03-24 20:25:29 +0800

    '时间计算'

--- dic.py
+++ dic.py
@@ -1,5 +1,5 @@
 # dictionary 提取文字的个数
-
+import time
 def average_count(filename):
 	data = []
 	count = 0
@@ -28,14 +28,19 @@ def average_count(filename):
 		if 'good' in d:
 			good.append(d)
 	print('一共有',len(good),'笔留言提到good')
+	print('-------------------------------------------------------')
+	print('data 内含内容')
+	return data
+
 
 		#print(newlen[])
 # 文字的计数以及查询
-def extract(filename):
-	with open(filename,'r',encoding = 'utf-8-sig') as d:
+def extract(filename,data):
+	with open(filename,'r',encoding = 'utf-8-sig') as f:
+		start_time = time.time()
 		wc = {}
-		for line in d:
-			words = line.split(' ')
+		for d in data:
+			words = d.split(' ')
 		for word in words:
 			if word in wc:
 				wc[word] += 1
@@ -46,6 +51,8 @@ def extract(filename):
 			if wc[word] > 100:
 				print(word,wc[word])
 		print('字数', len(wc))
+		end_time = time.time()
+		print('花了',end_time - start_time,'seconds')
 
 		while True:
 			word = input('请问你想要什么字：')
@@ -56,10 +63,11 @@ def extract(filename):
 				print(word, '出现过的次数为', wc[word])
 			else:
 				print('感谢使用,没有这个词')
+		print(wc)
 
 def main():
 
-	average_count('reviews.txt',)
-	words = extract('reviews.txt')
+	data = average_count('reviews.txt')
+	words = extract('reviews.txt', data)
 
 main()
